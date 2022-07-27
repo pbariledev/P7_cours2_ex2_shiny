bad956c0825bd13ba38fcf23c3802bfb515a0052
Pierre BARILE <pbariledev@gmail.com>
2022-07-27 16:59:51 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home/index'
 import Survey from './pages/Survey'
   
 ReactDOM.render(
-    <React.StrictMode>
-        <Router>
-            <Router path="/">
-                <Home />
-            </Router>
-            <Router path="/survey">
-                <Survey />
-            </Router>
-        </Router>
-    </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+      <Router>
+          <Route path="/">
+              <Home />
+          </Route>
+          <Route path="/survey">
+              <Survey />
+          </Route>
+      </Router>
+  </React.StrictMode>,
+document.getElementById('root')
 )
 
